060fce73ba92c6950d7e45a955c9ea231a322e8c
Ali Khatami <38954034+akhatami@users.noreply.github.com>
2024-09-05 11:15:15 +0200

    update publications

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -3,16 +3,33 @@ import  Navigation  from './Navigation';
 import './Publications.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faGoogle, faLinkedin} from "@fortawesome/free-brands-svg-icons";
-import {faBook} from "@fortawesome/free-solid-svg-icons";
+import {faBook, faGraduationCap} from "@fortawesome/free-solid-svg-icons";
 
 const Publications = () => {
     const publications = [
+        {
+            title: 'Software Quality Assurance Analytics: Enabling Software Engineers to Reflect on QA Practices',
+            authors: 'Ali Khatami, Carolin Brandt, Andy Zaidman',
+            venue: 'IEEE 24th International Working Conference on Source Code Analysis and Manipulation (SCAM)',
+            year: 2024,
+            link: 'https://azaidman.github.io/publications/khatamiSCAM2024a.pdf',
+            linkTitle: 'Pre-print',
+        },
+        {
+            title: 'Catching Smells in the Act: A GitHub Actions Workflow Investigation',
+            authors: 'Ali Khatami, Cédric Willekens, Andy Zaidman',
+            venue: 'IEEE 24th International Working Conference on Source Code Analysis and Manipulation (SCAM)',
+            year: 2024,
+            link: 'https://azaidman.github.io/publications/khatamiSCAM2024b.pdf',
+            linkTitle: 'Pre-print',
+        },
         {
             title: 'State‐of‐the‐Practice in Quality Assurance in Java‐Based Open Source Software Development',
             authors: 'Ali Khatami, Andy Zaidman',
             venue: 'Software: Practice and Experience',
             year: 2024,
             link: 'https://doi.org/10.1002/spe.3321',
+            linkTitle: 'DOI',
         },
         {
             title: 'Shaken, Not Stirred. How Developers Like Their Amplified Tests',
@@ -20,6 +37,7 @@ const Publications = () => {
             venue: 'IEEE Transactions on Software Engineering',
             year: 2024,
             link: 'https://doi.org/10.1109/TSE.2024.3381015',
+            linkTitle: 'DOI',
         },
         {
             title: 'Quality Assurance Awareness in Open Source Software Projects on GitHub',
@@ -27,6 +45,7 @@ const Publications = () => {
             venue: 'IEEE 23rd International Working Conference on Source Code Analysis and Manipulation (SCAM)',
             year: 2023,
             link: 'https://doi.org/10.1109/SCAM59687.2023.00027',
+            linkTitle: 'DOI',
         },
     ];
 
@@ -52,7 +71,7 @@ const Publications = () => {
                             </p>
                             <p>
                                 <a href={publication.link} target="_blank" rel="noopener noreferrer">
-                                    {publication.link}
+                                    {publication.linkTitle}
                                 </a>
                             </p>
                         </div>
@@ -65,7 +84,7 @@ const Publications = () => {
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
                         <a href="https://scholar.google.com/citations?user=ax4ieSsAAAAJ&hl=en&oi=sra" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faGoogle} />
+                            <FontAwesomeIcon icon={faGraduationCap} />
                         </a>
                     </div>
                 </div>
